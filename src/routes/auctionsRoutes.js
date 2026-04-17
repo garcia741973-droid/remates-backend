2b268d4fb34e3c69094f2663ff0abbd5c901ff23
carlos garcia <garcia1973@hotmail.com>
2026-04-17 16:13:50 -0400

    fix duplicate import auctionsRoutes

--- src/routes/auctionsRoutes.js
+++ src/routes/auctionsRoutes.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { createAuction, setCurrentLot, getAuctionById } = require('../controllers/auctionsController');
+
 const { requireAuth } = require('../middleware/authMiddleware');
 
-const { setCurrentLot } = require('../controllers/auctionsController');
-
+// crear remate
 router.post('/', requireAuth, createAuction);
 
+// obtener remate por id 🔥
 router.get('/:id', requireAuth, getAuctionById);
 
+// cambiar lote actual
 router.post('/current-lot', requireAuth, setCurrentLot);
 
 module.exports = router;
