d9bc9506846649f4a4dae3d5cc3f1ac1566054fa
carlos garcia <garcia1973@hotmail.com>
2026-04-18 14:29:35 -0400

    mensaje

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { login } = require('../controllers/authController');
+const { login, getUserCompanies } = require('../controllers/authController');
 
 router.post('/login', login);
-
 router.post('/get-companies', getUserCompanies);
 
 module.exports = router;
