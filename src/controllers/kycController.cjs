0f7a0ab72c84f090430f553d3b05cec74bb281d8
carlos garcia <garcia1973@hotmail.com>
2026-04-21 15:19:40 -0400

    mensaje

--- src/controllers/kycController.cjs
+++ src/controllers/kycController.cjs
@@ -210,21 +210,9 @@ const getPendingKyc = async (req, res) => {
 //
   const approveKyc = async (req, res) => {
     try {
-      const kycId = req.params.id;
+      const userId = req.params.userId;
 
       console.log("🔎 KYC ID:", kycId);
-
-      // 🔥 1. BUSCAR EL USER_ID REAL
-      const result = await pool.query(
-        `SELECT user_id FROM user_kyc WHERE id = $1`,
-        [kycId]
-      );
-
-      if (result.rows.length === 0) {
-        return res.status(404).json({ error: "KYC no encontrado" });
-      }
-
-      const userId = result.rows[0].user_id;
 
       console.log("✅ USER ID REAL:", userId);
 
