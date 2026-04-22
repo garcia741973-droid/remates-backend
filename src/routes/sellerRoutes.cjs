6dcb900e940441ef17de99daa17925b162b9675e
carlos garcia <garcia1973@hotmail.com>
2026-04-22 10:04:28 -0400

    mensaje

--- src/routes/sellerRoutes.cjs
+++ src/routes/sellerRoutes.cjs
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireAuth } = require("../middlewares/authMiddleware");
+const { requireAuth } = require("../middleware/authMiddleware");
 const { requestSeller } = require("../controllers/sellerController.cjs");
 
 router.post("/request", requireAuth, requestSeller);
