985eb74b2b7c21c2d5bb5f9347152a796e96b25e
carlos garcia <garcia1973@hotmail.com>
2026-04-27 19:22:52 -0400

    fix: import negotiationsController

--- src/routes/negotiationsRoutes.js
+++ src/routes/negotiationsRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const negotiationsController = require('../controllers/negotiationsController');
+
 const {
   createNegotiation,
   sendMessage,
