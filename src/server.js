fe072ce358de490ee8d6e2ebf8a1e3ceefb37afb
carlos garcia <garcia1973@hotmail.com>
2026-05-05 12:39:31 -0400

    mensaje

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ console.log("ENV:", process.env.FIREBASE_SERVICE_ACCOUNT ? "OK" : "NO EXISTE");
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n')
+  );
+
   admin.initializeApp({
-    credential: admin.credential.cert(
-      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-    ),
+    credential: admin.credential.cert(serviceAccount),
   });
 
   console.log("🔥 Firebase Admin inicializado");
