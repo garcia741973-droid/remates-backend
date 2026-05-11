7f2c30f73d7caa6cc2a4f3b4534a590da425afa2
carlos garcia <garcia1973@hotmail.com>
2026-05-11 16:05:18 -0400

    mensaje

--- src/routes/lotsRoutes.js
+++ src/routes/lotsRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const lotsController =
+  require('../controllers/lotsController');
+
 const { requireAuth } = require('../middleware/authMiddleware');
 
 const {
