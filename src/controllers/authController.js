fcdd515234b75c1b53f5c7d3dbfa66ae58987757
carlos garcia <garcia1973@hotmail.com>
2026-05-27 15:08:10 -0400

    mensaje

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -286,27 +286,11 @@ exports.registerParticipant =
 
       `
       SELECT
-
-        u.id,
-
-        u.email,
-
-        COALESCE(
-
-          uk.kyc_status,
-
-          u.kyc_status,
-
-          'not_started'
-
-        ) AS kyc_status
-
-      FROM users u
-
-      LEFT JOIN user_kyc uk
-      ON uk.user_id = u.id
-
-      WHERE u.email = $1
+        id,
+        email,
+        kyc_status
+      FROM users
+      WHERE email = $1
       `,
       [email]
     );
