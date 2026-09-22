f56d7ed3634e87b576badb34bc90707916f5779d
carlos garcia <garcia1973@hotmail.com>
2026-09-22 06:25:28 -0400

    mensaje

--- src/controllers/transportController.js
+++ src/controllers/transportController.js
@@ -4932,7 +4932,18 @@ const createDispatch = async (req, res) => {
         event_local_time ||
           new Date()
       ).toLocaleString(
-        'es-BO'
+        'es-BO',
+        {
+          timeZone:
+            'America/La_Paz',
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          hour12: false,
+        }
       );
 
     await client.query(
